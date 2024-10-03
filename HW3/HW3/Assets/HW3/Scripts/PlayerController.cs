22cba339c233af23a58831f8802ce7c3fe2634ca
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-10-03 23:40:26 +0900

    feature: prevent double jump of player

--- HW3/HW3/Assets/HW3/Scripts/PlayerController.cs
+++ HW3/HW3/Assets/HW3/Scripts/PlayerController.cs
@@ -6,23 +6,36 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
 
+    // Collision
+    private bool isGround = true;
+
     // Jump action
-    private float jumpForce = 600.0f;
+    private float jumpForce = 800.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= 1; 
+        Physics.gravity *= 2; 
     }
 
     // Update is called once per frame
     void Update()
     {
         // Jump action
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGround = false;
         }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Prevent double jump
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
     }
 }
