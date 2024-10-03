49413d658b738d3f9356e87fe47f31d2571d7a3e
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-10-04 02:26:27 +0900

    feature: added walking and jumping animation

--- HW3/HW3/Assets/HW3/Scripts/PlayerController.cs
+++ HW3/HW3/Assets/HW3/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnimator;
 
     // Collision
     private bool isGround = true;
@@ -14,33 +15,52 @@ public class PlayerController : MonoBehaviour
 
     // Move player
     private float moveSpeed = 8.0f;
-    private float forwardInput;
+    private float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= 2; 
+
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        forwardInput = Input.GetAxis("Horizontal");
+        // Get L/R key input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput == 0.0f)  {
+            // Set idle animation when no movement
+            playerAnimator.SetBool("Static_b", true);
+            playerAnimator.SetFloat("Speed_f", 0.0f);
+        } else {
+            // Set movement animation (walking)
+            playerAnimator.SetBool("Static_b", false);
+            playerAnimator.SetFloat("Speed_f", 0.3f);
+        }
 
         // Move player based on horizontal input
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwardInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * horizontalInput);
 
         // Jump action
         if (Input.GetKeyDown(KeyCode.Space) && isGround) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGround = false;
+
+            // Trigger jump animation
+            playerAnimator.SetTrigger("Jump_trig");
         }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Stop jump animation
+        playerAnimator.SetBool("Jump_b", false);
+
         // Prevent double jump
         if (collision.gameObject.CompareTag("Ground"))
         {
